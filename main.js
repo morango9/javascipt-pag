1476d7b9b919b17c269931ef58cc307b373b51eb
morango9 <105944114+morango9@users.noreply.github.com>
2023-10-06 11:55:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+// Pom-------------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
