6c53f90fd0158692dd03e836663e3a4cf4fbdcce
morango9 <105944114+morango9@users.noreply.github.com>
2023-11-24 11:42:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-// Pom-------------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (selectorAudio) {
+    const elementos = document.querySelector (selectorAudio);
+
+    if(elemento !=null && elemento.localName === 'audio'){
+        elemento.play ();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = documen.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento =tecla.classList[1];
+    const idAudio = `#Som_${instrumento}`; //template string
+
+tecla.onclik = function() {
+    tocaSomPom(idAudio);
+}
+
+ tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space'|| evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+    }
+
+ }
+
+ tecla.onkeyup = function (){
+    tecla.classList.remove('ativa');
+ }
+
+}
+
+
+
